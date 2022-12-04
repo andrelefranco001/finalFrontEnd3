eb07ca12138a17150e92541efa00028b6be011a0
Andrés Velásquez <andrelefranco001>
2022-12-03 22:45:17 -0500

    Creacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
 
 function App() {
   return (
+
       <div className="App">
-          <Navbar/>
-          <Footer/>
+          <Navbar />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/dentist" element={<Detail />} />
+              <Route path="/contacto" element={<Contact />} />
+              <Route path="/favs" element={<Favs />} />
+            </Routes>
+          </BrowserRouter>
+          <Footer />
       </div>
   );
 }
